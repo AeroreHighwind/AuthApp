4b5e8dc72ace58a596d9a8b9e23fac07b04e7dc6
Gaston Gallegos <ggallegos@mobydigital.com>
2023-09-27 13:47:04 -0300

    add swal2,login service

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2'
 
 @Component({
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+
+  private fb = inject(FormBuilder)
+  private authService = inject(AuthService)
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login() {
+    const { email, password } = this.myForm.value;
+    this.authService.login(email, password)
+      .subscribe({
+        next: () => console.log('all good'),
+        error: (errorMsg) => {
+          Swal.fire('Error', errorMsg, 'error')
+        }
+      });
+  }
 
 }
